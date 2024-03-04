e8f3086efcdb61664af708a33979f09dcbe17f5a
Yousef Shaban <jo.yousefshaban@gmail.com>
2024-03-04 17:31:30 +0000

    CH6 - Introduced different data bindings, and used some pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,32 +1,40 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+  selector: "pm-products",
+  templateUrl: "./product-list.component.html"
 })
 
 export class ProductListComponent {
-    pageTitle: string = "Product list";
-    products: any[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          },
-    ];
+  pageTitle: string = "Product list";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = "card";
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    },
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
