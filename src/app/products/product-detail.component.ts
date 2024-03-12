02ce179c531176a026056bf8736b09dcbe54d638
YousefShaban <yousef.shaban@omnibees.com>
2024-03-12 14:03:14 +0000

    CH12- Introduced guards and improved routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './IProduct';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
+  product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2021",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "assets/images/saw.png"
+    }
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
