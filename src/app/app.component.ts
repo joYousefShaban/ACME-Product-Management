7c7b55b9a22fc0d743f648fab5d5aead19fac73b
YousefShaban <yousef.shaban@omnibees.com>
2024-03-13 15:14:03 +0000

    CH13 - Divided app.module to multiple modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,8 @@ import { Component } from "@angular/core";
 })
 
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   pageTitle: string = 'Acme Product Managment';
 }
